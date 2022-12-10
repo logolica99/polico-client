4d9b17f870a1cc8998e302e51047dbe14e286126
Jubaer Jami <jamijubaer@gmail.com>
2022-12-11 01:27:51 +0600

    lazy loading component implemented

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
-import "./styles/style.scss";
-import Homepage from "./pages/Homepage";
-import ErrorPage from "./pages/ErrorPage";
-import LoginPage from "./pages/Login";
-import RegisterPage from "./pages/Register";
-import AccountPage from "./pages/my-account";
-
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./styles/style.scss";
+
+//importing pages
+const Homepage = lazy(() =>  import("./pages/Homepage"));
+const ErrorPage = lazy(() => import("./pages/ErrorPage"));
+const LoginPage = lazy(() => import("./pages/Login"));
+const RegisterPage = lazy(() => import("./pages/Register"));
+const AccountPage = lazy(() => import("./pages/my-account"));
+const CreateListing = lazy(() => import("./pages/create-listing"));
+const YourListing = lazy(() => import("./pages/your-listing"));
+const Wishlist = lazy(() => import("./pages/wishlist"));
+const Search = lazy(() => import("./pages/search"));
+
+//importing components
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
-import CreateListing from "./pages/create-listing";
-import YourListing from "./pages/your-listing";
-import Wishlist from "./pages/wishlist";
-import Search from "./pages/search";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -22,18 +25,21 @@ root.render(
     <BrowserRouter>
       <Nav />
       <div className="nav-padding"></div>
-      <Routes>
-        <Route path="*" element={<ErrorPage />} />
-        <Route path="/" element={<Homepage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/account" element={<AccountPage />} />
-        <Route path="/create-listing" element={<CreateListing />} />
-        <Route path="/listings" element={<YourListing />} />
-        <Route path="/wishlist" element={<Wishlist />} />
-        <Route path="/product/:query" element={<Search />} />
-      </Routes>
+      <Suspense fallback={<h2>Loading...</h2>}>
+        <Routes>
+          <Route path="*" element={<ErrorPage />} />
+          <Route path="/" element={<Homepage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/account" element={<AccountPage />} />
+          <Route path="/create-listing" element={<CreateListing />} />
+          <Route path="/listings" element={<YourListing />} />
+          <Route path="/wishlist" element={<Wishlist />} />
+          <Route path="/product/:query" element={<Search />} />
+        </Routes>
+      </Suspense>
       <Footer />
     </BrowserRouter>
   </React.StrictMode>
 );
+
