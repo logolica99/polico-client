18b8038b80e1b905145252939b8be8a90185f849
Jubaer Jami <jamijubaer@gmail.com>
2022-12-30 19:57:01 +0600

    added filter option in mobile search

--- src/pages/search/index.jsx
+++ src/pages/search/index.jsx
@@ -2,7 +2,25 @@ import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import ListingCard from "../../components/ListingCard";
 import Slider from "@mui/material/Slider";
-import { Checkbox, MenuItem, Pagination, Select } from "@mui/material";
+import {
+  Checkbox,
+  MenuItem,
+  Pagination,
+  Select,
+  Dialog,
+  Slide,
+  Button,
+} from "@mui/material";
+
+import ListItemText from "@mui/material/ListItemText";
+import ListItem from "@mui/material/ListItem";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import CloseIcon from "@mui/icons-material/Close";
 
 const productData = [
   {
@@ -34,6 +52,10 @@ const productData = [
   },
 ];
 
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
 export default function Search() {
   const maxPrice = 10000000;
   const [priceRange, setPriceRange] = useState([0, maxPrice]);
@@ -53,10 +75,85 @@ export default function Search() {
   const handlePriceRangeChange = (event, newValue) => {
     setPriceRange(newValue);
   };
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <div className="search margin-x">
-      <div className="search-filter">
+      <Dialog
+        fullScreen
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Transition}
+      >
+        {" "}
+        <AppBar sx={{ position: "relative", background: "#34A5C9" }}>
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              onClick={handleClose}
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+
+            <h3 style={{ flex: 1, color: "white", marginLeft: "20px" }}>
+              Search Filters
+            </h3>
+            <Button
+              autoFocus
+              color="inherit"
+              onClick={handleClose}
+              style={{ fontFamily: "montserrat", fontWeight: "bold" }}
+            >
+              Apply
+            </Button>
+          </Toolbar>
+        </AppBar>
+        <div className="search-filter">
+          <div className="search-filter-price">
+            <h3 className="search-filter-price-heading">Price Range</h3>
+            <div className="search-filter-price-slider">
+              <Slider
+                getAriaLabel={() => "Price range"}
+                value={priceRange}
+                max={maxPrice}
+                onChange={handlePriceRangeChange}
+                valueLabelDisplay="auto"
+                disableSwap
+              />
+            </div>
+            <div className="search-filter-price-range">
+              <div className="search-filter-price-range-box">
+                <p>{new Intl.NumberFormat("en-IN").format(priceRange[0])} </p>
+              </div>
+              <div className="search-filter-price-range-box">
+                <p>{new Intl.NumberFormat("en-IN").format(priceRange[1])} </p>
+              </div>
+            </div>
+          </div>
+          <div className="search-filter-location">
+            <h3 className="search-filter-location-heading">Location</h3>
+            <div className="search-filter-location-locations">
+              {locations.map((location) => (
+                <div className="search-filter-location-locations-item">
+                  <Checkbox />
+                  <p>{location} Division</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </Dialog>
+      <div className="search-filter hide-for-mobile">
         <div className="search-filter-price">
           <h3 className="search-filter-price-heading">Price Range</h3>
           <div className="search-filter-price-slider">
@@ -137,7 +234,7 @@ export default function Search() {
 
         <div className="search-content-advancedSearch ">
           <div></div>
-          <button>Advanced Search</button>
+          <button onClick={handleClickOpen}>Advanced Search</button>
         </div>
         <div className="search-content-cards">
           {productData.map((data) => (
